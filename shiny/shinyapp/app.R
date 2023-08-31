2ea8ec31c8016a124c15f37feaf49a04bbfa86fc
Chunlin Li <li000007@umn.edu>
2023-08-31 18:37:52 -0500

    update shiny

--- shiny/shinyapp/app.R
+++ shiny/shinyapp/app.R
@@ -1,215 +1,220 @@
+setwd("~/Documents/projects/sumdag/shiny/shinyapp")
 
 library(shiny)
+library(igraph)
+library(sumdag)
 
-# a button: add edges
-# two lists: from, to
-# this updates d_mat
-# a button: testing
+load("ppi.RData")
+p <- beta_mat %>% ncol() # 23
+protein <- colnames(beta_mat)
+snp <- rownames(beta_mat)
 
-# use a specific data
-# p-value adjustment slidebar (scale??) ??
-# a button: testing each single edges
+
+vertex.color <- rep("#7a0019", p)
 
 
 
 # Define UI for application
 ui <- fluidPage(
-    # App title ----
-    titlePanel("Inferring cardiovascular-related
-                protein-protein interaction network"),
-    fluidRow(
-        column(
-            3,
-            h3("Buttons"),
-            actionButton("action", "Action"),
-            br(),
-            br(),
-            submitButton("Submit")
-        ),
-        column(
-            3,
-            h3("Single checkbox"),
-            checkboxInput("checkbox", "Choice A", value = TRUE)
-        ),
-        column(
-            3,
-            checkboxGroupInput("checkGroup",
-                h3("Checkbox group"),
-                choices = list(
-                    "Choice 1" = 1,
-                    "Choice 2" = 2,
-                    "Choice 3" = 3
-                ),
-                selected = 1
-            )
-        ),
-        column(
-            3,
-            dateInput("date",
-                h3("Date input"),
-                value = "2014-01-01"
-            )
-        )
-    ),
-    fluidRow(
-        column(
-            3,
-            dateRangeInput("dates", h3("Date range"))
-        ),
-        column(
-            3,
-            fileInput("file", h3("File input"))
-        ),
-        column(
-            3,
-            h3("Help text"),
-            helpText(
-                "Note: help text isn't a true widget,",
-                "but it provides an easy way to add text to",
-                "accompany other widgets."
-            )
-        ),
-        column(
-            3,
-            numericInput("num",
-                h3("Numeric input"),
-                value = 1
-            )
-        )
-    ),
-    fluidRow(
-        column(
-            3,
-            radioButtons("radio", h3("Radio buttons"),
-                choices = list(
-                    "Choice 1" = 1, "Choice 2" = 2,
-                    "Choice 3" = 3
-                ), selected = 1
-            )
-        ),
-        column(
-            3,
-            selectInput("select", h3("Select box"),
-                choices = list(
-                    "Choice 1" = 1, "Choice 2" = 2,
-                    "Choice 3" = 3
-                ), selected = 1
-            )
-        ),
-        column(
-            3,
-            sliderInput("slider1", h3("Sliders"),
-                min = 0, max = 100, value = 50
-            ),
-            sliderInput("slider2", "",
-                min = 0, max = 100, value = c(25, 75)
-            )
-        ),
-        column(
-            3,
-            textInput("text", h3("Text input"),
-                value = "Enter text..."
-            )
-        )
-    ),
 
-    # Sidebar layout with input and output definitions ----
+    # Application title
+    titlePanel("Inferring cardiovascular-related protein-protein interaction network"),
+
+    # Sidebar layout with input and output definitions
     sidebarLayout(
 
-        # Sidebar panel for inputs ----
+        # Inputs
         sidebarPanel(
 
-            # Input: Slider for the number of bins ----
-            sliderInput(
-                inputId = "bins",
-                label = "Number of bins:",
-                min = 1,
-                max = 50,
-                value = 30
+            # Edge selection
+            # The choices can be changed to any list of nodes
+            selectInput("from_node", "From Node:", choices = protein),
+            selectInput("to_node", "To Node:", choices = protein),
+
+            # Add edge button
+            actionButton("add_edge", "Add Edge"),
+
+            # Type of testing
+            radioButtons("test_type", "Type of Testing:",
+                choices = c("edge", "path")
             ),
-            h2("Installation"),
-            p("Shiny is available on CRAN, so you can install it in the usual way from your R console:"),
-            code('install.packages("shiny")'),
-            br(),
-            br(),
-            br(),
-            br(),
-            br(),
-            "Shiny is a product of "
+
+            # Calculate button
+            actionButton("calculate", "Calculate")
         ),
 
-        # Main panel for displaying outputs ----
+        # Outputs
         mainPanel(
+            # Network plot
+            plotOutput("network_plot"),
 
-            # Output: Histogram ----
-            plotOutput(outputId = "distPlot")
+            # Output Panel
+            h3("Results"),
+            textOutput("likelihood_ratio"),
+            textOutput("degrees_freedom"),
+            textOutput("p_value")
         )
-        # mainPanel(
-        #     p("p creates a paragraph of text."),
-        #     p("A new p() command starts a new paragraph. Supply a style attribute to change the format of the entire paragraph.", style = "font-family: 'times'; font-si16pt"),
-        #     strong("strong() makes bold text."),
-        #     em("em() creates italicized (i.e, emphasized) text."),
-        #     br(),
-        #     code("code displays your text similar to computer code"),
-        #     div("div creates segments of text with a similar style. This division of text is all blue because I passed the argument 'style = color:blue' to div", style = "color:blue"),
-        #     br(),
-        #     p(
-        #         "span does the same thing as div, but it works with",
-        #         span("groups of words", style = "color:blue"),
-        #         "that appear inside a paragraph."
-        #     )
-        # )
     )
 )
 
 
-# Define server logic required to draw a histogram ----
-server <- function(input, output) {
 
-    # Histogram of the Old Faithful Geyser Data ----
-    # with requested number of bins
-    # This expression that generates a histogram is wrapped in a call
-    # to renderPlot to indicate that:
-    #
-    # 1. It is "reactive" and therefore should be automatically
-    #    re-executed when inputs (input$bins) change
-    # 2. Its output type is a plot
-    output$distPlot <- renderPlot({
-        x <- faithful$waiting
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+server <- function(input, output, session) {
 
-        hist(x,
-            breaks = bins, col = "#007bc2", border = "white",
-            xlab = "Waiting time to next eruption (in mins)",
-            main = "Histogram of waiting times"
+    # Initialize an empty adjacency matrix and graph
+    d_mat <- matrix(0, p, p)
+    colnames(d_mat) <- rownames(d_mat) <- protein
+    g <- graph_from_adjacency_matrix(d_mat, mode = "directed")
+    la <- layout.circle(g)
+
+
+    output$network_plot <- renderPlot({
+        # plot(g, edge.arrow.size = 0.5, edge.arrow.width = 0.5, edge.lty = 2) # edge.lty = 2 for dashed lines
+
+        plot(g,
+            layout = layout.circle, vertex.label = "", main = "",
+
+            # === vertex
+            # vertex.color = "darkred", # Node color
+            # vertex.frame.color = "darkred", # Node border color
+            vertex.color = vertex.color,
+            vertex.frame.color = vertex.color,
+            vertex.shape = "circle", # One of "none", "circle", "square", "csquare", "rectangle" "crectangle", "vrectangle", "pie", "raster", or "sphere"
+            vertex.size = 10, # Size of the node (default is 15)
+
+            # === Edge
+            edge.color = "#000000", # Edge color
+            edge.width = 0.5, # Edge width, defaults to 1
+            edge.arrow.size = 0.5, # Arrow size, defaults to 1
+            edge.arrow.width = 0.5, # Arrow width, defaults to 1
+            edge.lty = 2 # Line type, could be 0 or "blank", 1 or "solid", 2 or "dashed", 3 or "dotted", 4 or "dotdash", 5 or "longdash", 6 or "twodash"
         )
+
+
+        ## Apply labels manually
+        # Specify x and y coordinates of labels, adjust outward as desired
+        x <- la[, 1] * 1.2
+        y <- la[, 2] * 1.2
+
+        # Create vector of angles for text based on number of nodes (flipping the orientation of the words half way around so none appear upside down)
+        angle <- ifelse(atan(-(la[, 1] / la[, 2])) * (180 / pi) < 0,
+            90 + atan(-(la[, 1] / la[, 2])) * (180 / pi),
+            270 + atan(-la[, 1] / la[, 2]) * (180 / pi)
+        )
+
+        # Apply the text labels with a loop with angle as srt
+        for (i in 1:length(x)) {
+            text(
+                x = x[i], y = y[i], labels = V(g)$name[i],
+                adj = NULL, pos = NULL, cex = 1, col = "black",
+                srt = angle[i], xpd = T
+            )
+        }
+    })
+
+    # Add Edge
+    observeEvent(input$add_edge, {
+        from <- input$from_node
+        to <- input$to_node
+
+        # Update adjacency matrix
+        if (from != to) {
+            d_mat[from, to] <- 1
+
+            # Update graph
+            g <<- add_edges(g, c(from, to))
+        }
+
+        # Plot the graph
+        output$network_plot <- renderPlot({
+            # plot(g, edge.arrow.size = 0.5, edge.arrow.width = 0.5, edge.lty = 2) # edge.lty = 2 for dashed lines
+
+            plot(g,
+                layout = layout.circle, vertex.label = "", main = "",
+
+                # === vertex
+                # vertex.color = "darkred", # Node color
+                # vertex.frame.color = "darkred", # Node border color
+                vertex.color = vertex.color,
+                vertex.frame.color = vertex.color,
+                vertex.shape = "circle", # One of "none", "circle", "square", "csquare", "rectangle" "crectangle", "vrectangle", "pie", "raster", or "sphere"
+                vertex.size = 10, # Size of the node (default is 15)
+
+                # === Edge
+                edge.color = "#000000", # Edge color
+                edge.width = 0.5, # Edge width, defaults to 1
+                edge.arrow.size = 0.5, # Arrow size, defaults to 1
+                edge.arrow.width = 0.5, # Arrow width, defaults to 1
+                edge.lty = 2 # Line type, could be 0 or "blank", 1 or "solid", 2 or "dashed", 3 or "dotted", 4 or "dotdash", 5 or "longdash", 6 or "twodash"
+            )
+
+
+            ## Apply labels manually
+            # Specify x and y coordinates of labels, adjust outward as desired
+            x <- la[, 1] * 1.2
+            y <- la[, 2] * 1.2
+
+            # Create vector of angles for text based on number of nodes (flipping the orientation of the words half way around so none appear upside down)
+            angle <- ifelse(atan(-(la[, 1] / la[, 2])) * (180 / pi) < 0,
+                90 + atan(-(la[, 1] / la[, 2])) * (180 / pi),
+                270 + atan(-la[, 1] / la[, 2]) * (180 / pi)
+            )
+
+            # Apply the text labels with a loop with angle as srt
+            for (i in 1:length(x)) {
+                text(
+                    x = x[i], y = y[i], labels = V(g)$name[i],
+                    adj = NULL, pos = NULL, cex = 1, col = "black",
+                    srt = angle[i], xpd = T
+                )
+            }
+        })
+    })
+
+    # Perform Hypothesis Testing
+    observeEvent(input$calculate, {
+
+        # convert the nonzero entries in d_mat to pairs
+        edges <- which(d_mat != 0, arr.ind = TRUE)
+
+        # convert pairs to a list of edges
+        pairs <- list()
+        for (i in 1:nrow(edges)) {
+            pairs[[i]] <- c(edges[i, 1], edges[i, 2])
+        }
+
+
+
+        # Assume causal_inference function returns a list of significant edges
+        results <- sumdag::causal_inference(d_mat, test_type = input$test_type)
+
+        # Update graph to highlight significant edges
+        # Assuming results$significant_edges is a list of significant edges in the format c("from", "to")
+        E(g)$lty <- ifelse(get.edge.ids(g, results$significant_edges), 1, 2) # edge.lty = 1 for solid lines
+        E(g)$color <- ifelse(get.edge.ids(g, results$significant_edges), "red", "black")
+
+        # Plot the updated graph
+        output$network_plot <- renderPlot({
+            #plot(g, edge.arrow.size = 0.5, edge.arrow.width = 0.5)
+
+
+
+
+        })
+
+        # Display the results
+        output$likelihood_ratio <- renderText({
+            paste("Likelihood Ratio:", results$likelihood_ratio)
+        })
+        output$degrees_freedom <- renderText({
+            paste("Degrees of Freedom:", results$df)
+        })
+        output$p_value <- renderText({
+            paste("P-value:", results$p_value)
+        })
     })
 }
 
 
 # Run the app ----
 shinyApp(ui = ui, server = server)
-
-
-
-ui <- fluidPage(
-  selectInput("dataset", "Dataset", c("diamonds", "rock", "pressure", "cars")),
-  conditionalPanel( condition = "output.nrows",
-                    checkboxInput("headonly", "Only use first 1000 rows"))
-)
-server <- function(input, output, session) {
-  datasetInput <- reactive({
-    switch(input$dataset,
-           "rock" = rock,
-           "pressure" = pressure,
-           "cars" = cars)
-  })
-
-  output$nrows <- reactive({
-    nrow(datasetInput())
-  })
-
-  outputOptions(output, "nrows", suspendWhenHidden = FALSE)
-}
-
-shinyApp(ui, server)
